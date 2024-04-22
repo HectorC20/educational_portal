04401dd9911c7816cb09db694e98d50e33516cee
HECTORC20 <hector.08.cabrera@gmail.com>
2024-04-22 17:02:38 -0500

    Email add

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {EmailComponent} from "./email/email.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    EmailComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
